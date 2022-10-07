6a3c08c68072ba0c6ee08594285715734baea8a5
ming <guiguishou@yeah.net>
2022-10-07 12:19:27 +0800

    同步

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,15 +1,11 @@
 import { Navigate } from "react-router-dom";
 import Terminal from "../pages/terminal";
-import Test from "../pages/test";
 
 
 export const route = [
     {
         path: '/index',
         element: <Terminal />
-    }, {
-        path: '/test',
-        element: <Test />
     }, {
         path: '*',
         element: <Navigate to="/index"  />
